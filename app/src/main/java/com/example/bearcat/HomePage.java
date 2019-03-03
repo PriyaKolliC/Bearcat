91af23b5c4d231c0565a1b5d911e576b1314389b
Priya Kolli <lakshmipriya.kolli1@gmail.com>
2019-03-02 21:18:09 -0500

    login_home_qrpage

---
+++ app/src/main/java/com/example/bearcat/HomePage.java
@@ -0,0 +1,31 @@
+package com.example.bearcat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Button issue = findViewById(R.id.issue);
+
+        issue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.i("button_clicks","Report Issue Clicked");
+                Intent i=new Intent(HomePage.this, ScanQrCode.class);
+                startActivity(i);
+                Log.i("button_clicks","1");
+
+            }
+        });
+
+    }
+}
