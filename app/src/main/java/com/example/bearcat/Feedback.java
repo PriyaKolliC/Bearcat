5db0eb6b5fb4560dbeb955e90c6066e0eddc7365
Nithyasri Babu <babuni@mail.uc.edu>
2019-03-03 09:47:38 -0500

    Edits on Push API

--- app/src/main/java/com/example/bearcat/Feedback.java
+++ app/src/main/java/com/example/bearcat/Feedback.java
@@ -1,5 +1,8 @@
 package com.example.bearcat;
 
+import com.example.bearcat.server.Api;
+import com.example.bearcat.server.AsyncResponse;
+
 import android.content.Intent;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Button;
 import android.app.AlertDialog;
+
+import java.util.ArrayList;
+
+import static android.icu.lang.UCharacter.toUpperCase;
 
 public class Feedback extends AppCompatActivity {
 
@@ -24,41 +31,58 @@ public class Feedback extends AppCompatActivity {
 
         String[] split_labels = qr_string.split("-");
         final String building = split_labels[2];
-        final String roomnumber = split_labels[1];
-        final String roomtype = split_labels[0];
+        final String roomNumber = split_labels[1];
+        final String roomType = split_labels[0];
 
-        if(roomtype.equals("class"))
+        if(roomType.equals("class"))
             setContentView(R.layout.activity_feedback);
         else
             setContentView(R.layout.activity_feedback__washroom);
 
         TextView Title = (TextView)findViewById(R.id.Title);
-        Title.setText(building + " " + roomnumber + " " + roomtype);
+        Title.setText(toUpperCase(building + " " + roomNumber));
 
-        final LinearLayout ll = (LinearLayout)findViewById(R.id.Room);
+        TextView Type = (TextView) findViewById(R.id.Type);
+            if(roomType.equals("class"))
+                Type.setText(toUpperCase("classroom"));
+            else
+                Type.setText(toUpperCase("washroom"));
+
+            final LinearLayout ll = (LinearLayout)findViewById(R.id.Room);
 
         Button sub = findViewById(R.id.submit_button);
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog ad = new AlertDialog.Builder(Feedback.this).create();
-                ad.setTitle("Feedback Received");
+                String value_inSeries = "" + building + "-" + roomNumber + "-" + roomType;
 
-                String value_inSeries = "" + building + "-" + roomnumber + "-" + roomtype;
+                ArrayList<Boolean> flags = new ArrayList<Boolean>();
 
                 final int childCount = ll.getChildCount();
                 for(int i=0;i<childCount;i++){
                     View each_child = ll.getChildAt(i);
                     if(each_child instanceof CheckBox){
-                        if(((CheckBox) each_child).isChecked())
+                        if(((CheckBox) each_child).isChecked()) {
                             value_inSeries += "-" + "true";
-                        else
+                            flags.add(true);
+                        }
+                        else {
                             value_inSeries += "-" + "false";
+                            flags.add(false);
+                        }
                     }
 
                     if(each_child instanceof EditText)
                         value_inSeries += "-" + ((EditText) each_child).getText();
                 }
+
+                if(roomType.equals("class"))
+                    Api.postDataClass(building,roomNumber,roomType,flags);//,AsyncResponse<Boolean>);
+                else
+                    Api.postDataWash(building,roomNumber,roomType,flags);//,AsyncResponse<Boolean>);
+
+                ad.setTitle("Feedback Sent");
 
                 ad.setMessage(value_inSeries);
                 ad.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
--- app/src/main/java/com/example/bearcat/server/Api.java
+++ app/src/main/java/com/example/bearcat/server/Api.java
@@ -4,24 +4,24 @@ import android.net.Uri;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import java.util.ArrayList;
 
 public class Api {
     private String ENDPOINT_URI = "destress.appspot.com";
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     OkHttpClient client = new OkHttpClient();
 
-    private Uri.Builder getUriBuilder() {
+    private static Uri.Builder getUriBuilder() {
         return new Uri.Builder()
                 .scheme("https")
                 .authority(ENDPOINT_URI);
     }
 
-    public void authenticate(String username, String password, AsyncResponse<Boolean> asyncResponse) throws JSONException {
+    public static void authenticate(String username, String password, AsyncResponse<Boolean> asyncResponse) throws JSONException {
         String url = getUriBuilder()
                     .appendPath("authenticate")
                     .build()
@@ -40,19 +40,31 @@ public class Api {
         postTask.execute();
     }
 
-    public void postData(String building,
+    public static void postDataClass(String building,
                          String roomNumber,
                          String roomType,
-                         Boolean powerSocketBroken,
-                         Boolean switchLightsOff,
-                         Boolean tooHot,
-                         Boolean tooCold,
-                         Boolean dirtyRoom,
+                         ArrayList<Boolean> flags,
                          AsyncResponse<Boolean> asyncResponse) throws JSONException {
+
+        Boolean powerSocketBroken=false,switchLightsOff=false,tooHot=false,tooCold=false,dirtyRoom=false,Other=false;
+
+        for(int i = 0 ;i<flags.size();i++){
+            if(flags.get(i)){
+                switch(i){
+                    case 0: powerSocketBroken = true; break;
+                    case 1: switchLightsOff = true; break;
+                    case 2: tooHot = true; break;
+                    case 3: tooCold = true; break;
+                    case 4: dirtyRoom = true; break;
+                    case 5: Other = true; break;
+                }
+            }
+        }
         String url = getUriBuilder()
                     .appendPath("postData")
                     .build()
                     .toString();
+
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("building", building);
         jsonObject.put("roomNumber", roomNumber);
@@ -62,6 +74,52 @@ public class Api {
         jsonObject.put("tooHot", tooHot);
         jsonObject.put("tooCold", tooCold);
         jsonObject.put("dirtyRoom", dirtyRoom);
+        jsonObject.put("Other",Other);
+        RequestBody requestBody = RequestBody.create(JSON, jsonObject.toString());
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        ApiTask postTask = new ApiTask(request, asyncResponse);
+        postTask.execute();
+    }
+
+    public static void postDataWash(String building,
+                                    String roomNumber,
+                                    String roomType,
+                                    ArrayList<Boolean> flags,
+                                    AsyncResponse<Boolean> asyncResponse) throws JSONException {
+
+        Boolean WaterCold=false,TrashFull=false,Unclean=false,RefillPaper=false,Slippery=false,Other=false;
+        for(int i = 0 ;i<flags.size();i++){
+            if(flags.get(i)){
+                switch(i){
+                    case 0: WaterCold = true; break;
+                    case 1: TrashFull = true; break;
+                    case 2: Unclean = true; break;
+                    case 3: RefillPaper = true; break;
+                    case 4: Slippery = true; break;
+                    case 5: Other = true; break;
+                }
+            }
+        }
+        String url = getUriBuilder()
+                .appendPath("postData")
+                .build()
+                .toString();
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("building", building);
+        jsonObject.put("roomNumber", roomNumber);
+        jsonObject.put("roomType", roomType);
+        jsonObject.put("WaterCold", WaterCold);
+        jsonObject.put("TrashFull", TrashFull);
+        jsonObject.put("Unclean", Unclean);
+        jsonObject.put("RefillPaper", RefillPaper);
+        jsonObject.put("Slippert", Slippery);
+        jsonObject.put("Other",Other);
         RequestBody requestBody = RequestBody.create(JSON, jsonObject.toString());
         Request request = new Request.Builder()
                 .url(url)
